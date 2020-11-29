66613ae93c82409604e61546c58999d9bcb8f169
ParisiAndrea <74612033+ParisiAndrea@users.noreply.github.com>
2020-11-29 12:14:33 +0100

    Update colorZapper_test.R

--- R/colorZapper_test.R
+++ R/colorZapper_test.R
@@ -15,13 +15,45 @@ CZopen(path = cz_file)
 # associate files with the opened file
 CZaddFiles(dir)
 
-# define 1 point per image
-CZdefine(points = 1)
+#define ROIs  
+CZdefine(polygons = 1, marks = c('bill', 'crown', 'standard_grey', 'standard_white'))
+
+#extract RGB values from the ROI 
+CZextractROI()
+
+# fetch the extracted data from the SQLITE file 
+fetched_data = CZdata(what = 'ROI')  
 
 # check status
 CZshowStatus()
 
+#now, for each ID, each RGB value for mark=='bill' & == 'crown' needs to be converted using the RGB value with mark=='standard_grey' for that same ID  
 
+#vectorise grey regions 
+grey=subset(fetched_data, fetched_data$mark=='standard_grey') 
+
+#vectorise bill 
+bill=subset(fetched_data, fetched_data$mark=='bill') 
+
+#vectorise crown 
+crown=subset(fetched_data, fetched_data$mark=='crown') 
+
+#colour calibration
+for(i in length(fetched_data$id)) 
+{ 
+  bill$R=bill$R/(mean(grey$R/((grey$R+grey$G+grey$B)/3))) 
+  bill$G=bill$G/(mean(grey$G/((grey$R+grey$G+grey$B)/3))) 
+  bill$B=bill$B/(mean(grey$B/((grey$R+grey$G+grey$B)/3)))   
+
+  crown$R=crown$R/(mean(grey$R/((grey$R+grey$G+grey$B)/3))) 
+  crown$G=crown$G/(mean(grey$G/((grey$R+grey$G+grey$B)/3))) 
+  crown$B=crown$B/(mean(grey$B/((grey$R+grey$G+grey$B)/3))) 
+ 
+} 
+  
+#check outputs 
+summary(bill) 
+summary(crown)
 
 
 #------------------------------------------------------------------------------------------------------------------------

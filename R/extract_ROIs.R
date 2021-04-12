2487d3d65f64870fc05cd502736b4d5428d315d8
krietsch <jkrietsch@orn.mpg.de>
2021-04-12 10:31:50 +0200

    script to extract ROIs - error in evaluating the argument

---
+++ R/extract_ROIs.R
@@ -0,0 +1,22 @@
+#========================================================================================================================
+# Extract ROIs from pictures
+#========================================================================================================================
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'colorZapper', 'here'),
+        require, character.only = TRUE)
+
+# set working directory
+path_pictures = '//ds/raw_data_kemp/FIELD/Barrow/REPH_BODY_PICTURES/DATA_RENAMED_lowres/front/'
+
+# set the directory containing the existing SQLITE file (e.g., front)
+cz_file = './DATA/REPH_front.sqlite'
+
+# open the SQLITE file
+CZopen(path = cz_file)  
+
+# add pictures from the folder (e.g., front)
+CZaddFiles(path_pictures)
+
+# extract the RGB in the ROIs
+CZextractROI(parallel = FALSE) # parallel FALSE/TRUE 
